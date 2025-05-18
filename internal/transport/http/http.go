2ec9e625002680d5b398ca6e3c73decca4c4eafd
Evgeny Putselovsky <E9315717@yandex.ru>
2025-05-18 22:23:00 +0300

    Enable CORS

--- internal/transport/http/http.go
+++ internal/transport/http/http.go
@@ -1,22 +1,20 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	swagger "github.com/swaggo/http-swagger/v2"
 
 	"github.com/Glawary/crypt/internal/swag"
 	"github.com/Glawary/crypt/internal/usecase"
-	http2 "github.com/Glawary/crypt/pkg/http"
+	"github.com/Glawary/crypt/pkg/http"
 )
 
 type Server struct {
 	cryptService *usecase.CryptService
 }
 
-func InitServer(cfg *http2.HttpConfig, cryptService *usecase.CryptService) (*http2.HTTPServer, error) {
+func InitServer(cfg *http.HttpConfig, cryptService *usecase.CryptService) (*http.HTTPServer, error) {
 	s := &Server{
 		cryptService: cryptService,
 	}
@@ -30,16 +28,15 @@ func InitServer(cfg *http2.HttpConfig, cryptService *usecase.CryptService) (*htt
 		router.Get("/list", s.ListCrypto)
 	})
 
-	server := http2.NewHTTPServer(cfg, r)
+	server := http.NewHTTPServer(cfg, r)
 	return server, nil
 }
 
 func useCors() *cors.Cors {
 	return cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
-		AllowedHeaders: []string{"Accept", "Content-Type"},
-		AllowedMethods: []string{
-			http.MethodGet,
-		},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{"*"},
+		MaxAge:         250,
 	})
 }
